9da62bc77ea0fef2d0165a4c71a1fb0e6ebef054
Jacon11 <jaceplayz159@gmail.com>
2026-07-14 01:22:02 -0400

    Changed up some stuff in JSFW.
    Replaced XAL library with JSFW and simplified main function.

--- JSFW-1.0/Examples/main.c
+++ JSFW-1.0/Examples/main.c
@@ -1,25 +1,8 @@
-#include <GLAL/GLALapi.h>
-#include <stdio.h>
+#include <JSFW/jsfw1.h>
 
-int main()
+int main(void) 
 {
-    XAL* root = initX11Display(XAL_INIT_VIDEO, 0);
-    XAL* window = CreateX11Window(400, 400, "My XAL Application", 100, 200, COLOR_BLACK);
-
-    while (!X11WindowClosed())
-    {
-        X11Event e;
-        while (PollX11WindowEvents(&e))
-        {
-            if (e.type == X11_MOUSE_EVENT)
-            {
-                return true;
-            }
-        }
-
-        DisplayX11Window();
-    }
+    JSFWinit();
     
-    CloseX11();
     return 0;
 }
